358e4270edf74ac9e0e06e6ec59f58d434608a3d
jumatjai-create <jumatjai@gmail.com>
2026-05-10 10:36:17 +0300

    chore: sync fetch.js from trailer-ups

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { fileTypeFromBuffer } from '../lib/filetype-compat.js';
+import { fileTypeFromBuffer } from 'file-type';
 export default {
     command: 'fetch',
     aliases: ['get', 'download'],
